01752ce016abd38ee236b4d712bc11b0c51fc5a1
andre <andre@DESKTOP-FFV65RU>
2021-04-28 16:20:59 +0200

    .

--- GSILab/src/Presentacion/Tablon.java
+++ GSILab/src/Presentacion/Tablon.java
@@ -45,35 +45,61 @@ public class Tablon extends JPanel {
 		panelTablon.removeAll();
 		panelTablon.setVisible(true);
 		if(opcion.equals("Explorar")) {
-			for(int i = 0; i < publicaciones.length; i++) {
-				if (!publicaciones[i].getUsuario().equals(usuario.getNombre())) {
-					PublicacionAspecto pubAux = new PublicacionAspecto();
-					pubAux.setPropiedades(publicaciones[i].getUsuario(), publicaciones[i].getEtiqueta(),
-							publicaciones[i].getMensaje(),usuario);
-					panelTablon.add(pubAux);
-				}
-			}
-		} else if(opcion.equals("Notificaciones")) {
-			for(int i = 0; i < 20; i++) {
-				JLabel labelAux = new JLabel();
-				labelAux.setText("ESTO ES UNA PRUEBA");
-				panelTablon.add(labelAux);
-			}
-		} else if(opcion.equals("Tus publicaciones")){
-			for(int i = 0; i < publicaciones.length; i++) {
-				if (publicaciones[i].getUsuario().equals(usuario.getNombre())) {
-					PublicacionAspecto pubAux = new PublicacionAspecto();
-					pubAux.setPropiedades(publicaciones[i].getUsuario(), publicaciones[i].getEtiqueta(),
-							publicaciones[i].getMensaje(),usuario);
-					panelTablon.add(pubAux);
-				}
-			}
-		} else {
 			
+			/*
+			 * Cuando cambiemos esto que ahora muestra lo del boton de inicio, esto tiene que mostrar las publicaciones que tengan
+			 * las categorías con el MG  que  estan en el json, y luego tb tienen que no mostrar las publicaciones del usuario logueado
+			 * seria raro que salieran mis propias publicaciones
+			 * 
+			 */
+			mostrarHome();
+			mostrarExplorar();
+		} else if(opcion.equals("Notificaciones")) {
+			mostrarNotificaciones();
+			
+		} else if(opcion.equals("Tus publicaciones")){
+			mostrarMisPublicaciones();
+		} else {
+			mostrarHome();
 		}
 	}
 	public static void setUsuario(Usuario u) throws JSONException {
 		usuario = u;
 		publicaciones = GestorPublicaciones.leerPublicacionesExplorar();
 	}
+	
+	public static void mostrarExplorar() {
+		
+	}
+	
+	public static void mostrarHome() {
+		for(int i = 0; i < publicaciones.length; i++) {
+			if (!publicaciones[i].getUsuario().equals(usuario.getNombre())) {
+				PublicacionAspecto pubAux = new PublicacionAspecto();
+				pubAux.setPropiedades(publicaciones[i].getUsuario(), publicaciones[i].getEtiqueta(),
+						publicaciones[i].getMensaje(),usuario);
+				panelTablon.add(pubAux);
+			}
+		}
+	}
+	
+	public static void mostrarNotificaciones() {
+		for(int i = 0; i < 20; i++) {
+			JLabel labelAux = new JLabel();
+			labelAux.setText("ESTO ES UNA PRUEBA");
+			panelTablon.add(labelAux);
+		}
+	}
+	
+	public static void mostrarMisPublicaciones() {
+		for(int i = 0; i < publicaciones.length; i++) {
+			if (publicaciones[i].getUsuario().equals(usuario.getNombre())) {
+				PublicacionAspecto pubAux = new PublicacionAspecto();
+				pubAux.setPropiedades(publicaciones[i].getUsuario(), publicaciones[i].getEtiqueta(),
+						publicaciones[i].getMensaje(),usuario);
+				panelTablon.add(pubAux);
+			}
+		}
+	}
+	
 }
